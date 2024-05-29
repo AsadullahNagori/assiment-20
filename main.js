a92a1cd505bdd39d774ebcb0a5bab863ca826198
AsadullahNagori <164639593+AsadullahNagori@users.noreply.github.com>
2024-05-29 16:05:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+//Think of something you could store in a array.
+// For example, you could make a list of mountains, rivers, countries, cities, languages,
+// or anything else you’d like. Write a program that creates a list containing these items.*/
+let locations = ["Mount Everest", "Ravi", "Madina"];
+console.log(`\n list of locations:`);
+for (let i = 0; i < locations.length; i++) {
+    console.log(locations[i]);
+}
